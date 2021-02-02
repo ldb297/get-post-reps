e0981b6919a8fcd47a480da64255c8f22ad3c94b
Leonidas <ldbrooks297@gmail.com>
2021-02-02 15:30:47 -0800

    index: add delete route to server and button to view, cleared dinosaurs.json testing buttons

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ const ejs = require('ejs');
 const expressLayouts = require('express-ejs-layouts');
 const fs = require('fs');
 const { json } = require('express');
+const methodOverride = require('method-override');
 
 //middleware
 app.use(expressLayouts);
 app.use(express.urlencoded({extended: false}));
+app.use(methodOverride('_method'))
 app.set('view engine', 'ejs');
 
 
@@ -20,31 +22,32 @@ app.get('/', (req,res)=>{
 //post route, doesn't have view
 app.post('/dinosaurs', (req,res)=>{
     //pulled in file
-    let dinos = fs.readFileSync('./dinosaurs.json')
+    let dinos = fs.readFileSync('./dinosaurs.json');
     //made readable
-    dinos = JSON.parse(dinos)
+    dinos = JSON.parse(dinos);
     //created variable
-    const dino = dinos[req.params.index]
+    const dino = dinos[req.params.index];
     //created skeleton for form inputs to store
     const newDino = {
         name: req.body.name,
         type: req.body.type
     }
     //pushed into preexisting array
-    dinos.push(newDino)
+    dinos.push(newDino);
     //rewrites original file with new data
-    fs.writeFileSync('./dinosaurs.json', JSON.stringify(dinos))
+    fs.writeFileSync('./dinosaurs.json', JSON.stringify(dinos));
     //get requests us back to home page
-    res.redirect('/dinosaurs')
+    let lastIndex = dinos.length - 1
+    res.redirect(`/dinosaurs/${lastIndex}`);
 })
 
 //index view
 app.get('/dinosaurs', (req,res)=>{
-    let dinos = fs.readFileSync('./dinosaurs.json')
+    let dinos = fs.readFileSync('./dinosaurs.json');
     //translates data
-    dinos = JSON.parse(dinos)
-    console.log(req.query.nameFilter)
-    let nameToFilterBy = req.query.nameFilter
+    dinos = JSON.parse(dinos);
+    console.log(req.query.nameFilter);
+    let nameToFilterBy = req.query.nameFilter;
     
     if(nameToFilterBy){
         const newFilteredArray = dinos.filter((dinosaurObj)=>{
@@ -54,22 +57,34 @@ app.get('/dinosaurs', (req,res)=>{
         })
         dinos = newFilteredArray
     }
-    
     //renders page in views folder
-    res.render('dinosaurs/index', {dinos: dinos})
+    res.render('dinosaurs/index', {dinos: dinos});
 })
 
 //new view
 app.get('/dinosaurs/new', (req,res)=>{
-    res.render('dinosaurs/new')
+    res.render('dinosaurs/new');
 })
 
 //show view
 app.get('/dinosaurs/:index', (req,res)=>{
-    let dinos = fs.readFileSync('./dinosaurs.json')
-    dinos = JSON.parse(dinos)
-    const dino = dinos[req.params.index]
-    res.render('dinosaurs/show', {dino})
+    let dinos = fs.readFileSync('./dinosaurs.json');
+    dinos = JSON.parse(dinos);
+    const dino = dinos[req.params.index];
+    res.render('dinosaurs/show', {dino});
+})
+
+//removing dinosaurs
+app.delete('/dinosaurs/:idx', (req,res)=>{
+    const dinosaurs = fs.readFileSync('./dinosaurs.json');
+    const dinosaursArray = JSON.parse(dinosaurs);
+
+    let idx = Number(req.params.idx);
+
+    dinosaursArray.splice(idx, 1);
+    fs.writeFileSync('./dinosaurs.json', JSON.stringify(dinosaursArray))
+    
+    res.redirect('/dinosaurs')
 })
 
 //local server hosting
